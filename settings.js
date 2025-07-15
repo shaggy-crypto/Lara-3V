b8dae236672a4b908446463fc98d7b22bfdcdeb2
shaggy-crypto <kalpanaumesh2314@gmail.com>
2025-07-15 15:40:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CUZ3CDba#xITE0LxwCNQ9SwhLgfYSNZAX_Okp1-EnVD58GQj9Crs", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "KAgTwLxC#EBNv0trP_UNf2J7lKyXhI8_Oavz6dd5tVzfHy1WTv2M", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
